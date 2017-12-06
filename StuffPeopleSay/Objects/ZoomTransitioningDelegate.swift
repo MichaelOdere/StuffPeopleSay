6df4a4858510183c5daacd5501f35e198d3fa926
Michael Odere <michael@odereinc.com>
2017-12-05 17:49:42 -0800

    Working on zoom transition class

--- StuffPeopleSay/Objects/ZoomTransitioningDelegate.swift
+++ StuffPeopleSay/Objects/ZoomTransitioningDelegate.swift
@@ -15,6 +15,25 @@ class ZoomTransitioningDelegate:NSObject{
     var operation: UINavigationControllerOperation = .none
     private let zoomScale = CGFloat(15)
     private let backgroundScale = CGFloat(0.7)
+    
+    typealias ZoomingViews = (collectionView: UICollectionView, imageView: UIView)
+    
+    func configureViews(for state: TransitionState, containerView: UIView, backgroundViewController: UIViewController, viewsInBackground: ZoomingViews, viewsInForeground: ZoomingViews, snapshotViews: ZoomingViews){
+        
+        switch state {
+        case .initial:
+            backgroundViewController.view.transform = CGAffineTransform.identity
+            backgroundViewController.view.alpha = 1
+            
+            snapshotViews.collectionView.frame = containerView.convert(viewsInBackground.collectionView.frame, from: viewsInBackground.collectionView.superview)
+        
+        case .final:
+                backgroundViewController.view.transform = CGAffineTransform(scaleX: backgroundScale, y: backgroundScale)
+                backgroundViewController.view.alpha = 0
+            
+                snapshotViews.collectionView.frame = containerView.convert(viewsInForeground.collectionView.frame, from: viewsInForeground.collectionView.superview)
+        }
+    }
 }
 
 extension ZoomTransitioningDelegate: UIViewControllerAnimatedTransitioning{
@@ -24,8 +43,9 @@ extension ZoomTransitioningDelegate: UIViewControllerAnimatedTransitioning{
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         let duration = transitionDuration(using: transitionContext)
-        let fromVC = transitionContext.viewController(forKey: .from)
-        let toVC = transitionContext.viewController(forKey: .to)
+        let fromVC = transitionContext.viewController(forKey: .from)!
+        let toVC = transitionContext.viewController(forKey: .to)!
+        let containerView = transitionContext.containerView
         
         var backgroundVC = fromVC
         var foregroundVC = toVC
@@ -33,6 +53,60 @@ extension ZoomTransitioningDelegate: UIViewControllerAnimatedTransitioning{
         if operation == .pop{
             backgroundVC = toVC
             foregroundVC = fromVC
+        }
+        
+        let maybeBackgroundView = (backgroundVC as? ZoomViewController)?.zoomingCollectionView(for: self)
+        let maybeForegroundView = (foregroundVC as? ZoomViewController)?.zoomingCollectionView(for: self)
+
+        assert(maybeBackgroundView != nil, "Cannot find view in background")
+        assert(maybeForegroundView != nil, "Cannot find view in foreground")
+        
+        let backgroundView = maybeBackgroundView!
+        let foregroundView = maybeForegroundView!
+        
+        let viewSnapshot:UICollectionView = UICollectionView(frame: backgroundView.frame)
+        viewSnapshot.delegate = backgroundView.delegate
+        viewSnapshot.dataSource = backgroundView.dataSource
+        viewSnapshot.layer.masksToBounds = true
+        
+        backgroundView.isHidden = true
+        foregroundView.isHidden = true
+        
+        let foregroundViewBackgroundColor = foregroundVC.view.backgroundColor
+        foregroundVC.view.backgroundColor = UIColor.clear
+        containerView.backgroundColor = UIColor.white
+        
+        containerView.addSubview(backgroundVC.view)
+        containerView.addSubview(foregroundVC.view)
+        containerView.addSubview(viewSnapshot)
+
+        var preTransitionState = TransitionState.initial
+        var postTransitionState = TransitionState.final
+        
+        if operation == .pop{
+            preTransitionState = .final
+            postTransitionState = .initial
+
+        }
+        
+        configureViews(for: preTransitionState, containerView: containerView, backgroundViewController: backgroundVC, viewsInBackground: (backgroundView, backgroundView), viewsInForeground: (foregroundView,foregroundView), snapshotViews: (viewSnapshot, viewSnapshot))
+        
+        foregroundVC.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: [], animations: {
+            
+            self.configureViews(for: postTransitionState, containerView: containerView, backgroundViewController: backgroundVC, viewsInBackground: (backgroundView, backgroundView), viewsInForeground: (foregroundView,foregroundView), snapshotViews: (viewSnapshot, viewSnapshot))
+       
+        }) { (finished) in
+            backgroundVC.view.transform = CGAffineTransform.identity
+            
+            viewSnapshot.removeFromSuperview()
+            backgroundView.isHidden = false
+            foregroundView.isHidden = false
+            
+            foregroundVC.view.backgroundColor = foregroundViewBackgroundColor
+            
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
         }
     }
 }
