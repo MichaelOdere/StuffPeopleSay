1d2980ef5fe3af2055504c76ec6aa37aba24c565
Michael Odere <michael@odereinc.com>
2018-01-29 10:08:23 -0800

    Spacing

--- StuffPeopleSayTests/BingoBoardTests.swift
+++ StuffPeopleSayTests/BingoBoardTests.swift
@@ -17,12 +17,10 @@ class StuffPeopleSayTests: XCTestCase {
     func testHorizontalWinConditions() {
         // 1. given clear board
         bingoBoard.boardReset()
-        
         // 2. when we create horizontal win conditions
         for row in 0..<bingoBoard.size {
             for col in 0..<bingoBoard.size {
                 bingoBoard.board[row][col] = 1
-                
                 if col == bingoBoard.size - 1 {
                     // 3. then when we reach the end of the row we should have a win
                     XCTAssertEqual(bingoBoard.checkVictory(x: row, y: col), true, "Win condition failed")
@@ -31,7 +29,6 @@ class StuffPeopleSayTests: XCTestCase {
                     XCTAssertEqual(bingoBoard.checkVictory(x: row, y: col), false, "No win condition failed")
                 }
             }
-            
             // 5. clear the board so we can check the next row
             bingoBoard.boardReset()
         }
@@ -40,12 +37,10 @@ class StuffPeopleSayTests: XCTestCase {
     func testVerticalWinConditions() {
         // 1. given clear board
         bingoBoard.boardReset()
-        
         // 2. when we create vertical win conditions
         for col in 0..<bingoBoard.size {
             for row in 0..<bingoBoard.size {
                 bingoBoard.board[row][col] = 1
-                
                 if row == bingoBoard.size - 1 {
                     // 3. then when we reach the end of the column we should have a win
                     XCTAssertEqual(bingoBoard.checkVictory(x: row, y: col), true, "Win condition failed")
@@ -54,7 +49,6 @@ class StuffPeopleSayTests: XCTestCase {
                     XCTAssertEqual(bingoBoard.checkVictory(x: row, y: col), false, "No win condition failed")
                 }
             }
-
             // 5. clear the board so we can check the next column
             bingoBoard.boardReset()
         }
@@ -63,14 +57,12 @@ class StuffPeopleSayTests: XCTestCase {
     func testSlantLeftConditions() {
         // 1. given clear board
         bingoBoard.boardReset()
-
         // 2. when we create slant left win conditions
         for index in 0..<bingoBoard.size {
             // 3. then when we have not reached the end of the slant we should not have a win
             XCTAssertEqual(bingoBoard.checkVictory(x: index, y: index), false, "No win condition failed")
             bingoBoard.board[index][index] = 1
         }
-    
         // 4. then when we reach the end of the slant we should have a win
         XCTAssertEqual(bingoBoard.checkVictory(x: bingoBoard.size - 1, y: bingoBoard.size - 1), true, "Win condition failed")
     }
@@ -78,14 +70,12 @@ class StuffPeopleSayTests: XCTestCase {
     func testSlantRightConditions() {
         // 1. given clear board
         bingoBoard.boardReset()
-        
         // 2. when we create slant right win conditions
         for index in 0..<bingoBoard.size {
             // 3. then when we have not reached the end of the slant we should not have a win
             XCTAssertEqual(bingoBoard.checkVictory(x: index, y: index), false, "No win condition failed")
             bingoBoard.board[bingoBoard.size - index - 1][index] = 1
         }
-        
         // 4. then when we reach the end of the slant we should have a win
         XCTAssertEqual(bingoBoard.checkVictory(x: 0, y: bingoBoard.size - 1), true, "Win condition failed")
     }
