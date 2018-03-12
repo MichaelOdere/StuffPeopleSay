3f332ce90777f6e39b4503267e74aa684f82cf7d
Michael Odere <michael@odereinc.com>
2018-03-12 10:47:43 -0700

    Fixing Identifier Names

--- StuffPeopleSay/Stores/BingoPalette.swift
+++ StuffPeopleSay/Stores/BingoPalette.swift
@@ -1,19 +1,19 @@
 import UIKit
 
 public struct BingoPalette {
-    static public let backgroundColor = UIColor(r: 0, g: 0, b: 0)
-    static public let vanillaBackgroundColor = UIColor(r: 242, g: 242, b: 235)
-    static public let silverBackgroundColor = UIColor(r: 214, g: 215, b: 216)
-    static public let bingoCellBackgroundColor = UIColor(r: 54, g: 80, b: 98)
+    static public let backgroundColor = UIColor(red: 0, green: 0, blue: 0)
+    static public let vanillaBackgroundColor = UIColor(red: 242, green: 242, blue: 235)
+    static public let silverBackgroundColor = UIColor(red: 214, green: 215, blue: 216)
+    static public let bingoCellBackgroundColor = UIColor(red: 54, green: 80, blue: 98)
 
-    static public let SPSred = UIColor(r: 240, g: 93, b: 94)
-    static public let SPSgrey = UIColor(r: 231, g: 236, b: 239)
-    static public let SPSlightBlue = UIColor(r: 142, g: 220, b: 230)
-    static public let SPSgreen = UIColor(r: 85, g: 139, b: 110)
+    static public let SPSred = UIColor(red: 240, green: 93, blue: 94)
+    static public let SPSgrey = UIColor(red: 231, green: 236, blue: 239)
+    static public let SPSlightBlue = UIColor(red: 142, green: 220, blue: 230)
+    static public let SPSgreen = UIColor(red: 85, green: 139, blue: 110)
 }
 
 extension UIColor {
-    convenience init(r: Int, g: Int, b: Int) {
-        self.init(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1.0)
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
 }
