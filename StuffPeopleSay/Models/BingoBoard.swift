83d3c986a0615bd6033d599ed9c2ae2417a9a2fd
Michael Odere <michael@odereinc.com>
2018-03-12 11:15:14 -0700

    Formatting

--- StuffPeopleSay/Models/BingoBoard.swift
+++ StuffPeopleSay/Models/BingoBoard.swift
@@ -1,121 +1,116 @@
+// swiftlint:disable variable_name
 import Foundation
 
-class BingoBoard{
+class BingoBoard {
     enum DiagonalResult {
         case slantingLeft
         case slantingRight
         case both
         case neither
     }
-    
-    var board:[[Int]]!
+
+    var board: [[Int]]!
     var size = 5
-    
+
     init() {
         boardReset()
     }
-    
-    func cardsToBoard(cards: [Card]){
-        for index in 0..<cards.count{
+
+    func cardsToBoard(cards: [Card]) {
+        for index in 0..<cards.count {
             if index > 24 {
                 break
             }
             let card = cards[index]
             let x = index % size
-            let y:Int = index / size
-            if card.active{
+            let y: Int = index / size
+            if card.active {
                 board[x][y] = 1
             }
         }
     }
-
-    func checkVictory(x:Int, y: Int)->Bool{
+    
+    func checkVictory(x: Int, y: Int) -> Bool {
         var victory = false
-        if checkVertical(y: y) || checkHorizontal(x: x){
+        if checkVertical(y: y) || checkHorizontal(x: x) {
             victory = true
-        }else {
+        } else {
             let result = isDiagonal(x: x, y: y)
-            
-            if result != DiagonalResult.neither{
+
+            if result != DiagonalResult.neither {
                 victory = checkDiagonal(dr: result)
             }
         }
         return victory
     }
-    
-    func boardReset(){
+
+    func boardReset() {
         board = []
-        for _ in 0..<size{
+        for _ in 0..<size {
             let row = Array(repeatElement(0, count: size))
             board.append(row)
         }
     }
-    
-    private func checkHorizontal(x:Int)->Bool{
-        for elem in 0..<size{
-            if board[x][elem] != 1{
-                return false
-            }
+
+    private func checkHorizontal(x: Int) -> Bool {
+        for elem in 0..<size where board[x][elem] != 1 {
+            return false
         }
         return true
     }
-    
-    private func checkVertical(y: Int)->Bool{
-        for elem in 0..<size{
-            if board[elem][y] != 1{
-                return false
-            }
+
+    private func checkVertical(y: Int) -> Bool {
+        for elem in 0..<size where board[elem][y] != 1 {
+            return false
         }
         return true
     }
-    
-    private func checkDiagonal(dr: DiagonalResult)->Bool{
+
+    private func checkDiagonal(dr: DiagonalResult) -> Bool {
         var victory = false
         switch dr {
         case .both:
-            if checkLeftSlant() || checkRightSlant(){
+            if checkLeftSlant() || checkRightSlant() {
                 victory = true
             }
         case .slantingLeft:
             victory = checkLeftSlant()
-            
+
         case .slantingRight:
             victory = checkRightSlant()
-            
+
         default:
             victory = false
         }
         return victory
     }
-    
-    private func checkRightSlant()->Bool{
-        for elem in 0..<size{
+
+    private func checkRightSlant() -> Bool {
+        for elem in 0..<size {
             let x = 4 - elem
             let y = elem
-            if board[x][y] != 1{
+            if board[x][y] != 1 {
                 return false
             }
         }
         return true
     }
 
-    private func checkLeftSlant()->Bool{
-        for elem in 0..<size{
-            if board[elem][elem] != 1{
-                return false
-            }
+    private func checkLeftSlant() -> Bool {
+        for elem in 0..<size where board[elem][elem] != 1 {
+            return false
         }
         return true
     }
-    
-    private func isDiagonal(x:Int, y: Int)->DiagonalResult{
-        if x == y{
-            if x == 2{
+
+    private func isDiagonal(x: Int, y: Int) -> DiagonalResult {
+        if x == y {
+            if x == 2 {
                 return DiagonalResult.both
             }
             return DiagonalResult.slantingLeft
         }
-        if (x + y) == 4{
+        if (x + y) == 4 {
             return DiagonalResult.slantingRight
         }
         return DiagonalResult.neither
--- StuffPeopleSay/Models/Game.swift
+++ StuffPeopleSay/Models/Game.swift
@@ -3,12 +3,11 @@ import SwiftyJSON
 
 class Game {
     var gameId: String
-//    var name:String
     var status: String
     var my: User
     var opponents: [User]
 
-    init(gameId:String, status:String, my: User, opponents:[User]) {
+    init(gameId: String, status: String, my: User, opponents: [User]) {
         self.gameId = gameId
         self.status = status
         self.my = my
@@ -22,7 +21,7 @@ extension Game {
             print("Error parsing game object for key: gameId")
             return nil
         }
-        
+
         guard let status = json["status"].string else {
             print("Error parsing game object for key: status")
             return nil
@@ -32,16 +31,16 @@ extension Game {
             print("Error parsing game object for key: users")
             return nil
         }
-        
+
         guard let myData = usersData["my"] else {
             print("Error parsing game object for key: my")
             return nil
         }
 
-        var myUser:User!
+        var myUser: User!
         if let aUser = User(json: myData) {
             myUser = aUser
-        }else{
+        } else {
             return nil
         }
 
@@ -49,42 +48,41 @@ extension Game {
             print("Error parsing game object for key: opponents")
             return nil
         }
-        
-        var allOpponents:[User] = []
+
+        var allOpponents: [User] = []
         for opponentData in allOpponentsData {
-            if let opponent = User(json: opponentData){
+            if let opponent = User(json: opponentData) {
                 allOpponents.append(opponent)
             }
         }
 
         self.init(gameId: gameId, status: status, my: myUser, opponents: allOpponents)
     }
-    
-    func getOpponents()->String{
-        var opponentsText = ""
-        
-        switch self.opponents.count {
-            case 0:
-                opponentsText = "No Opponents"
-            case 1:
-                opponentsText = self.opponents[0].name
-            case 2:
-                opponentsText = self.opponents[0].name + " & " + self.opponents[1].name
-            case 3:
-                opponentsText = self.opponents[0].name + ", " + self.opponents[1].name + ", & " + self.opponents[2].name
-            default:
-                opponentsText = self.opponents[0].name + ", " + self.opponents[1].name + ", & " + String(self.opponents.count - 2) + " Opponents"
-        }
-        
-        if opponentsText.count > 42{
-            opponentsText = String(self.opponents.count) + " Opponents"
 
+    func getOpponents() -> String {
+        var opponentsText = ""
+
+        switch self.opponents.count {
+        case 0:
+            opponentsText = "No Opponents"
+        case 1:
+            opponentsText = self.opponents[0].name
+        case 2:
+            opponentsText = self.opponents[0].name + " & " + self.opponents[1].name
+        case 3:
+            opponentsText = self.opponents[0].name + ", " + self.opponents[1].name + ", & " + self.opponents[2].name
+        default:
+            opponentsText = self.opponents[0].name + ", " + self.opponents[1].name + ", & " + String(self.opponents.count - 2) + " Opponents"
+        }
+
+        if opponentsText.count > 42 {
+            opponentsText = String(self.opponents.count) + " Opponents"
         }
 
         return opponentsText
     }
-    
-    func getSatusColor()->UIColor{
+
+    func getSatusColor() -> UIColor {
         switch self.status {
         case "Playing":
             return UIColor.green
