4aeacfb7271c68fce2284d88b2d8f8e659e4f128
Michael Odere <michael@odereinc.com>
2018-03-12 10:57:22 -0700

    Formatting

--- StuffPeopleSay/Models/User.swift
+++ StuffPeopleSay/Models/User.swift
@@ -1,24 +1,22 @@
 import Foundation
 import SwiftyJSON
 
-struct User{
+struct User {
     var userId: String
     var name: String
     var boards: [Board] = []
-    
-    init(userId:String, name:String, boards:[Board]) {
+
+    init(userId: String, name: String, boards: [Board]) {
         self.userId = userId
         self.name = name
         self.boards = boards
     }
-    
-    func getCardsActive(index: Int)->Int{       
+
+    func getCardsActive(index: Int) -> Int {
         let deck = self.boards[index].boardDeck
         var count = 0
-        for card in deck.cards{
-            if card.active{
-                count += 1
-            }
+        for card in deck.cards where card.active {
+            count += 1
         }
         return count
     }
@@ -30,25 +28,24 @@ extension User {
             print("Error parsing user object for key: name")
             return nil
         }
-        
+
         guard let userId = json["userId"].string else {
             print("Error parsing user object for key: userId")
             return nil
         }
-        
-        guard let boardsData = json["boards"].array else{
+
+        guard let boardsData = json["boards"].array else {
             print("Error parsing user object for key: boards")
             return nil
         }
 
-        for boardData in boardsData{
-            if let tempBoard = Board(json: boardData){
+        for boardData in boardsData {
+            if let tempBoard = Board(json: boardData) {
                 self.boards.append(tempBoard)
             }
         }
-        
+
         self.name = name
         self.userId = userId
     }
 }
-
